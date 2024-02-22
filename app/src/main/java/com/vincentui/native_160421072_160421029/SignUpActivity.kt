1c22723b85f9d748c4aaf370399f38cc57b03fa6
cenz19 <86843417+cenz19@users.noreply.github.com>
2024-02-22 14:29:40 +0700

    Added Manual for Sign Up for New User

--- app/src/main/java/com/vincentui/native_160421072_160421029/SignUpActivity.kt
+++ app/src/main/java/com/vincentui/native_160421072_160421029/SignUpActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SignUpActivity : AppCompatActivity() {
+    /// SIGN UP FOR NEW USER
+    // 1. cek existing user
+    // 2. cek password
+    // 3. implement otp
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
