d8a413555b5e241f4ce1c215e5e6bf8b0444518e
cenz19 <86843417+cenz19@users.noreply.github.com>
2024-02-22 13:44:33 +0700

    First commit
    This is our first commit for setting up the Android Studio Project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "native_160421072_160421029"
+include(":app")
+ 
